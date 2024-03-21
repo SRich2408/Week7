33d91884df4748348868a685b800b7c035282fcf
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-03-21 14:54:05 -0400

    s

--- exercise10.java
+++ exercise10.java
@@ -1,3 +1,7 @@
+/* Name: Sebastian Richardson
+Project: GCD 
+Date: 3/21/2024 */
+
 import java.util.Scanner; 
 
 public class exercise10 {
@@ -7,13 +11,14 @@ public class exercise10 {
         int num1 = userInput.nextInt();
         System.out.println("Enter a second non negative integer: ");
         int num2 = userInput.nextInt();
-
+        userInput.close();
         while (num2 > 0) {
             int temp = num1 % num2;
             num1 = num2;
             num2 = temp;
         }
         System.out.println("The GCD is: " + num1);
+
 
         
         
