831c3083c0ca0288a57162ab0c0dd0843ce5bf87
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-03-21 14:20:47 -0400

    Create exercise10.java

---
+++ exercise10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+
+public class exercise10 {
+    public static void main (String[] args){
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter a non negative integer: ");
+        int num1 = userInput.nextInt();
+        System.out.println("Enter a second non negative integer: ");
+        int num2 = userInput.nextInt();
+
+        
+        
+
+    }
+}
